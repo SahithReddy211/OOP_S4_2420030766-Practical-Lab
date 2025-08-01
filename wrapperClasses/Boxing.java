b00993ea0e62bec9343937df0f7facd8e67700ab
V Sahith Reddy <vankidisahithreddy@gmail.com>
2025-08-01 20:48:36 +0530

    Successfully uploaded Wrapper Classes

---
+++ wrapperClasses/Boxing.java
@@ -0,0 +1,14 @@
+package wrapperClasses;
+
+public class Boxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Boxing
+		int a = 10;
+		Integer i=Integer.valueOf(a);
+		System.out.println("Integer: "+i);
+
+	}
+
+}
---
+++ wrapperClasses/Unboxing.java
@@ -0,0 +1,13 @@
+package wrapperClasses;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Unboxing
+		Integer i = 25;
+		int a = i.intValue();
+		System.out.println(a);
+	}
+
+}
