bd2f563a367c7bbc66a6e7559b1332fe3d83da12
V Sahith Reddy <vankidisahithreddy@gmail.com>
2025-08-01 20:43:03 +0530

    Successfully added Wrapper classes

---
+++ AutoBoxing.java
@@ -0,0 +1,15 @@
+package wrapperClasses;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Auto Boxing
+		int a = 10;
+		Integer i=a;
+		System.out.println("Integer: "+i);
+
+
+	}
+
+}
---
+++ AutoUnboxing.java
@@ -0,0 +1,14 @@
+package wrapperClasses;
+
+public class AutoUnboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Auto Unboxing
+		Integer i = 25;
+		int a = i;
+		System.out.println(a);
+
+	}
+
+}
