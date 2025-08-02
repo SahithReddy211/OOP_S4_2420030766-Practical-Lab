d83bc0541e504b2c58eba1bea8b708a4efe27e94
V Sahith Reddy <vankidisahithreddy@gmail.com>
2025-08-02 11:24:18 +0530

    Uploaded strings related problems

---
+++ strings/ReadAndPrint.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ReadAndPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//program to read string value and print it on the console
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any string : ");
+		String str  = sc.nextLine();
+		System.out.println("This is your string: "+str);
+		
+
+	}
+
+}
---
+++ strings/StringObject.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.Scanner;
+
+public class StringObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method
+		//program to display string value through string object
+		Scanner sc = new Scanner(System.in);
+		String str = new String("Hi! This is sahith.");
+				System.out.println(str);
+
+	}
+
+}
